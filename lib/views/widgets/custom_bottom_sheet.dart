91108629db26dcc56a32fb72286de947a27c7982
Hamza <mahmoudrhamza20@gmail.com>
2022-12-14 14:00:01 +0200

    create custom text field

--- lib/views/widgets/custom_bottom_sheet.dart
+++ lib/views/widgets/custom_bottom_sheet.dart
@@ -1,11 +1,43 @@
 
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: const [
+          SizedBox(height: 20,),
+          CustomTextField(),
+        ],
+      ),
+    );
+  }
+}
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'Tittle',
+        border: buildOutlineBorder(),
+        enabledBorder: buildOutlineBorder(),
+        focusedBorder: buildOutlineBorder(kPrimaryColor)
+      ),
+    );
+  }
+
+  OutlineInputBorder buildOutlineBorder([ color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide:  BorderSide(color: color ?? Colors.white),
+      );
   }
 }
