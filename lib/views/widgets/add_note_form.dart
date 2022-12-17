19e1cfd822a62bad8ae7ed071a6fbc1e370970f5
Hamza <mahmoudrhamza20@gmail.com>
2022-12-17 19:27:53 +0200

    create Colors list view in bottom sheet part two

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -5,6 +5,7 @@ import 'package:notes_app/models/note_model.dart';
 import 'custom_button.dart';
 import 'custom_text_field.dart';
 import 'package:intl/intl.dart';
+
 class AddNoteForm extends StatefulWidget {
   const AddNoteForm({
     Key? key,
@@ -45,20 +46,26 @@ class _AddNoteFormState extends State<AddNoteForm> {
               subTitle = value;
             },
           ),
-          const SizedBox(height: 16,),
+          const SizedBox(
+            height: 16,
+          ),
           const ColorsListView(),
-          const SizedBox(height: 16,),
+          const SizedBox(
+            height: 16,
+          ),
           BlocBuilder<AddNoteCubit, AddNoteState>(
             builder: (context, state) {
               return CustomButton(
-                isLoading: state is AddNoteLoading ? true :false,
+                isLoading: state is AddNoteLoading ? true : false,
                 text: 'Add',
                 onTap: () {
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!.save();
                     DateTime currentDate = DateTime.now();
-                    var formattedCurrentDate = DateFormat.MMMMd().format(currentDate);
-                    var note = NoteModel(title: title!,
+                    var formattedCurrentDate =
+                        DateFormat.MMMMd().format(currentDate);
+                    var note = NoteModel(
+                        title: title!,
                         subTitle: subTitle!,
                         date: formattedCurrentDate,
                         color: Colors.blue.value);
@@ -81,30 +88,70 @@ class _AddNoteFormState extends State<AddNoteForm> {
 }
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({Key? key}) : super(key: key);
+  const ColorItem({Key? key, required this.isSelected, required this.color}) : super(key: key);
+  final bool isSelected;
+  final Color color ;
 
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      radius: 25,
-      backgroundColor: Colors.blue,
-    );
+    return isSelected
+        ?  CircleAvatar(
+      radius: 23,
+      backgroundColor: Colors.white,
+          child: CircleAvatar(
+              radius: 20,
+              backgroundColor: color,
+            ),
+        )
+        :  CircleAvatar(
+            radius: 20,
+            backgroundColor: color,
+          );
   }
 }
-class ColorsListView extends StatelessWidget {
+
+class ColorsListView extends StatefulWidget {
   const ColorsListView({Key? key}) : super(key: key);
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = const[
+    Color(0xffEF7C8E),
+    Color(0xffFAE8E0),
+    Color(0xffB6E2D3),
+    Color(0xffD8A7B1),
+    Color(0xff2E765E),
+    Color(0xff638C80),
+    Color(0xff887BB0),
+    Color(0xffFFF4BD),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 25*2,
+      height: 25 * 2,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 8,
+        itemCount: colors.length,
         itemBuilder: (context, index) {
-        return const ColorItem();
-      },),
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: InkWell(
+                onTap: () {
+                  currentIndex = index;
+                  setState(() {});
+                },
+                child:  ColorItem(
+                  isSelected: currentIndex == index,
+                  color: colors[index],
+                )),
+          );
+        },
+      ),
     );
   }
 }
-
